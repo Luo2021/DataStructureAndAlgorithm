1d43a41e08e25201cc716bda70ce9685760b9a0e
luoli3 <Ll-19911220>
2021-10-27 20:06:17 +0800

    算法-二分查找

---
+++ algorithm/src/main/java/luo/li/binarySearch/BinarySearchDemo1.java
@@ -0,0 +1,37 @@
+package luo.li.binarySearch;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给定一个n个元素有序的（升序）整型数组nums 和一个目标值target，写一个函数搜索nums中的 target，
+ * 如果目标值存在返回下标，否则返回 -1
+ * @author luoli
+ * @date 2021/9/30 18:06
+ */
+public class BinarySearchDemo1 {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7);
+        int target = 47;
+        BinarySearchDemo1 binarySearchDemo1 = new BinarySearchDemo1();
+        System.out.println(binarySearchDemo1.search(nums,target));
+    }
+
+    int search(List<Integer> nums, int target) {
+        return BinarySearch(nums,target,0,nums.size()-1);
+    }
+
+    int BinarySearch(List<Integer> nums, int target, int left, int right){
+        if(left > right){
+            return -1;
+        }
+        int mid = (left + right )/2;
+        if(nums.get(mid) == target) {
+            return mid;
+        }else if(nums.get(mid) > target) {
+            return BinarySearch(nums,target,left, mid-1);
+        }else{
+            return BinarySearch(nums,target,mid+1, right);
+        }
+    }
+}
